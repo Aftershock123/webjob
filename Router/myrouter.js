49935fa519e390004f8ed6b62138e7669d14b97c
Aftershock123 <nonjirawat2506@gmail.com>
2023-04-14 02:03:01 +0700

    public immg css js

--- Router/myrouter.js
+++ Router/myrouter.js
@@ -1,26 +1,5 @@
-const express =require('express')
-const router =express.Router
-const path =require('path')
+const express = require('express')
+const router = express.Router
+const path = require('path')
 
-const home = path.join(__dirname,"../webpage/home.html")
-const login = path.join(__dirname,"../webpage/login.html")
-const registercompany = path.join(__dirname,"../webpage/registerCompany.html")
-
-router.get("/",(req,res)=>{
-    res.status(200)
-    res.type("text/html")
-    res.sendFile(home)
-})
-
-
-router.get("/login",(req,res)=>{
-    res.status(200)
-    res.type("text/html")
-    res.sendFile(login)
-})
-
-router.get("/registeruser",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../webpage/registerUser.html"))
-})
-
-module.exports =router
+module.exports = router 
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express =require('express')
-const router =require('./Router/myrouter')
+const path = require('path')
 const app = express()
 
-app.use(router)
+app.use(express.static(path.join(__dirname,'public')))
+
+
 
 app.listen(8080,()=>{
     console.log("server port 8080")
