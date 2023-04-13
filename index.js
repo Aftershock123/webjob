7288b51fcae98488ab2d17f378549e7d84a646ad
Aftershock123 <nonjirawat2506@gmail.com>
2023-04-13 16:58:36 +0700

    router

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-const http = require('http')
-const fs = require('fs')
-const url =require('url')
+const express =require('express')
+const router =require('./Router/myrouter')
+const app = express()
 
-const indexpage = fs.readFileSync(`${__dirname}/webpage/Home.html`)
-const registerUser = fs.readFileSync(`${__dirname}/webpage/registerUser.html`)
+app.use(router)
 
-const server =http.createServer((req,res)=>{
-    const pathName = req.url
-    if(pathName ==="/" || pathName ==="home"){
-        res.end(indexpage)
-        // res.end(registerUser)
-    }else if(pathName ==="/abc"){
-        res.end("<h1>hello abc</h1>")
-    }else {
-        res.writeHead(404)
-        res.end("<h1>Not found</h1>")
-    }
-
-})
-.listen(8080,'localhost',()=>{
-console.log("start 8080")
+app.listen(8080,()=>{
+    console.log("server port 8080")
 })
