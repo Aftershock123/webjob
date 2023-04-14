f1f24922582a980a2dc4ff3dbd38ec49fcd60f6f
Aftershock123 <nonjirawat2506@gmail.com>
2023-04-15 01:14:16 +0700

    html tran to ejs

--- Router/myrouter.js
+++ Router/myrouter.js
@@ -1,5 +1,25 @@
 const express = require('express')
-const router = express.Router
-const path = require('path')
+const router = express.Router()
+
+router.get('/',(req,res)=>{
+    const name ="Non"
+    res.render('index.ejs',{data:name})
+})
+
+
+router.get('/login',(req,res)=>{
+    res.render('login')
+})
+
+router.get('/registeruser',(req,res)=>{
+    
+    res.render('registeruser')
+})
+router.get('/registercompany',(req,res)=>{
+    
+    res.render('registercompany')
+})
+
+
 
 module.exports = router 
--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express =require('express')
 const path = require('path')
+const router =require('./Router/myrouter')
 const app = express()
 
+
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','ejs')
+app.use(router)
 app.use(express.static(path.join(__dirname,'public')))
 
 
