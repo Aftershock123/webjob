fc3634c7929e63eeb707917e21a9c3875a8573db
Aftershock123 <nonjirawat2506@gmail.com>
2023-05-17 02:49:15 +0700

    connect database

---
+++ Data/data.js
@@ -0,0 +1,6 @@
+// const {MySql_Port, MySql_Db} =process.env;
+
+// const dbString ="mysql://Non:1234@localhost:3306/Projectweb";
+// console.log(process.env);
+
+
--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 const express =require('express')
-const path = require('path')
-const router =require('./Router/myrouter')
+// const path = require('path')
+const mysql =require('mysql2');
+// const router =require('./Router/myrouter')
 const app = express()
 
 
-app.set('views',path.join(__dirname,'views'))
-app.set('view engine','ejs')
-app.use(router)
-app.use(express.static(path.join(__dirname,'public')))
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    database:'projectweb',
+    port:'3307'
+
+})
+connection.connect((err)=>{
+    if(err){
+        console.log('Error connect to mysql database = ',err)
+        return;
+    }
+    console.log('Mysql successfully connected');
+})
+
+// app.set('views',path.join(__dirname,'views'))
+// app.set('view engine','ejs')
+// app.use(router)
+// app.use(express.static(path.join(__dirname,'public')))
 
 
 
 app.listen(8080,()=>{
-    console.log("server port 8080")
+    console.log("server port 3030 ")
 })
